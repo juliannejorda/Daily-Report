eb55e135564f64f0d24f860f33c4c10eff062163
Julianne Jorda <julianne.jorda@yahoo.com>
2021-04-23 11:56:16 -0400

    fixing up the styles

--- src/components/InitalForm.jsx
+++ src/components/InitalForm.jsx
@@ -1,58 +1,62 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Formik, Form, Field } from 'formik';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import PropTypes from "prop-types";
+import { Formik, Form, Field } from "formik";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
 // import { Header } from './Header';
-import { Button } from '@material-ui/core';
-import * as yup from 'yup';
+import { Button } from "@material-ui/core";
+import * as yup from "yup";
 import Container from "react-bootstrap/Container";
 import { StylesProvider } from "@material-ui/core/styles";
-import { withStyles } from '@material-ui/core/styles';
-import ParticleEffectButton from 'react-particle-effect-button'
+import { withStyles } from "@material-ui/core/styles";
+import ParticleEffectButton from "react-particle-effect-button";
 import { motion } from "framer-motion";
 import particlesConfig from "./particlesConfig";
 import Particles from "react-particles-js";
-import { Typewriter } from 'react-typewriting-effect';
-import 'react-typewriting-effect/dist/index.css';
-import Fade from "react-reveal/Fade"
+import { Typewriter } from "react-typewriting-effect";
+import "react-typewriting-effect/dist/index.css";
+import Fade from "react-reveal/Fade";
+import "./InitalForm.css";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   form: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     // background: "linear-gradient(to left bottom, #3494e6, #7a8ce7, #ab82dc, #d176c8, #ec6ead)",
     background: "#F0F8FF",
     opacity: "0.9",
-    width: "800px",
+    width: "40rem",
+    //  width: "auto",
+    margin: "auto",
     // marginLeft: "22%",
-    marginLeft: "22vw",
+    //marginLeft: "22vw",
     // padding: "5% 0"
     // height: ""
     borderRadius: "35px",
-    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-    marginTop: "10px"
+    boxShadow:
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    marginTop: "10px",
   },
   button: {
     // margin: theme.spacing(1),
     // background: 'blue',
     // padding: "5%",
     margin: "3%",
-  boxShadow: "inset 0px 1px 0px 0px #85c1f5",
-  background: "linear-gradient(to bottom, #61b0ff 5%, #80b5ea 100%)",
-	backgroundColor: "#61b0ff",
-	borderRadius: "6px",
-	border: "1px solid #84bbf3",
-	display: "inline-block",
-	color: "#ffffff",
-	fontSize: "20px",
-	fontWeight: "bold",
-	padding: "8px 30px",
-	textDecoration: "none",
-	textShadow: "0px 1px 0px #528ecc",
+    boxShadow: "inset 0px 1px 0px 0px #85c1f5",
+    background: "linear-gradient(to bottom, #61b0ff 5%, #80b5ea 100%)",
+    backgroundColor: "#61b0ff",
+    borderRadius: "6px",
+    border: "1px solid #84bbf3",
+    display: "inline-block",
+    color: "#ffffff",
+    fontSize: "20px",
+    fontWeight: "bold",
+    padding: "8px 30px",
+    textDecoration: "none",
+    textShadow: "0px 1px 0px #528ecc",
   },
   // inputText: {
   //   border: '1px solid rgba(252, 252, 252, 0.4)',
@@ -66,55 +70,63 @@ const useStyles = makeStyles(theme => ({
   // }
   introText: {
     fontSize: "4rem",
-    fontFamily:  "'Playfair Display', serif",
-    verticalAlign: "middle"
+    fontFamily: "'Playfair Display', serif",
+    verticalAlign: "middle",
   },
   mainText: {
-    marginTop: "10%",
+    //  marginTop: "10%",
     textAlign: "center",
-  
   },
   smallText: {
     fontWeight: "600",
     fontStyle: "italic",
-    fontSize: "20px"
-  }
+    fontSize: "20px",
+    textAlign: "center",
+  },
 }));
 
 const validationSchema = yup.object({
-    name: yup.string()
+  name: yup
+    .string()
     .min(2, "Must be at least 2 characters")
     .max(50, "Name can't be longer than 50 characters")
     .required("*Name is Required"),
-    city: yup.string()
-    .min(1, 'Enter valid city')
-    .required('*Enter a valid city')
-  });
-  const state = {
-    hidden: false
-  }
+  city: yup.string().min(1, "Enter valid city").required("*Enter a valid city"),
+});
+const state = {
+  hidden: false,
+};
 
-  const FormUserDetails = ({ formData, setFormData, nextStep }) => {
+const FormUserDetails = ({ formData, setFormData, nextStep }) => {
   const classes = useStyles();
   return (
-<>
-<Particles 
-// style={{position: "absolute"}} height="100vh" width="100vw" 
-// style={{
-//       position: "absolute",
-//       top: 0,
-//       left: 0,
-//       width: "100%",
-//       height: "100%"
-//     }}
-params={particlesConfig} />
-<div  style={{
+    <>
+      <Particles
+        // style={{position: "absolute"}} height="100vh" width="100vw"
+        // style={{
+        //       position: "absolute",
+        //       top: 0,
+        //       left: 0,
+        //       width: "100%",
+        //       height: "100%"
+        //     }}
+        params={particlesConfig}
+      />
+      <div
+        style={{
+          // position: "absolute",
+          // top: 0,
+          // left: 0,
+          // width: "100%",
+          // height: "100%",
+          display: "table",
           position: "absolute",
           top: 0,
           left: 0,
+          height: "100%",
           width: "100%",
-          height: "100%"
-        }} >
+        }}
+      >
         {/* <motion.div
   // style={{
   //         width: "150px",
@@ -131,60 +143,60 @@ params={particlesConfig} />
     delay: 0.5
   }}
   > */}
-  <div className={classes.mainText}>
-   <Typewriter
-      string='Hello There...'
-      delay={120}
-      className={classes.introText}
-      id="introMainFont"
-      stopBlinkinOnComplete
-    />
-  <br></br>
-    <Typewriter
-      string='Please complete the following form:'
-      delay={70}
-      stopBlinkinOnComplete
-      className={classes.smallText}
-    />
-    </div>
-    {/* <h1 className={classes.introText}>Hello there</h1> */}
-  {/* </motion.div> */}
-  <Fade bottom>
-      <StylesProvider injectFirst>
-      {/* <Header title='Enter User Details' /> */}
-      <div className="forForm">
-      <Formik
-        initialValues={formData}
-        onSubmit={values => {
-          setFormData(values);
-          nextStep();
-        }}
-        validationSchema={validationSchema}
-      >
-        {({ errors, touched }) => (
-          <Form className={classes.form}>
-          
-            <Field
-              className="inputField"
-              name='name'
-              label='Enter Name'
-              margin='normal'
-              size="large"
-              as={TextField}
-              error={touched.name && errors.name}
-              helperText={touched.name && errors.name}
+        <div className="leBody">
+          <div className={classes.mainText}>
+            <Typewriter
+              string="Hello There..."
+              delay={120}
+              className={classes.introText}
+              id="introMainFont"
+              stopBlinkinOnComplete
             />
-            <Field
-            className="inputField"
-              name='city'
-              label='Enter city'
-              margin='normal'
-              size="large"
-              as={TextField}
-              error={touched.city && errors.city}
-              helperText={touched.city && errors.city}
+            <br></br>
+            <Typewriter
+              string="Please complete the following form:"
+              delay={70}
+              stopBlinkinOnComplete
+              className={classes.smallText}
             />
-            {/* <motion.button
+          </div>
+          {/* <h1 className={classes.introText}>Hello there</h1> */}
+          {/* </motion.div> */}
+          <Fade bottom>
+            <StylesProvider injectFirst>
+              {/* <Header title='Enter User Details' /> */}
+              <div className="forForm">
+                <Formik
+                  initialValues={formData}
+                  onSubmit={(values) => {
+                    setFormData(values);
+                    nextStep();
+                  }}
+                  validationSchema={validationSchema}
+                >
+                  {({ errors, touched }) => (
+                    <Form className={classes.form}>
+                      <Field
+                        className="inputField"
+                        name="name"
+                        label="Enter Name"
+                        margin="normal"
+                        size="large"
+                        as={TextField}
+                        error={touched.name && errors.name}
+                        helperText={touched.name && errors.name}
+                      />
+                      <Field
+                        className="inputField"
+                        name="city"
+                        label="Enter city"
+                        margin="normal"
+                        size="large"
+                        as={TextField}
+                        error={touched.city && errors.city}
+                        helperText={touched.city && errors.city}
+                      />
+                      {/* <motion.button
             whileHover={{ scale: 1.1 }}
     whileTap={{ scale: 0.9 }} >
             <Button
@@ -200,36 +212,37 @@ params={particlesConfig} />
               Submit
             </Button>
             </motion.button> */}
-            <motion.button 
-            whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }} 
-    type='submit'   variant='contained'
-    className={classes.button}
+                      <motion.button
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                        type="submit"
+                        variant="contained"
+                        className={classes.button}
+                      >
+                        Submit
+                      </motion.button>
 
-    >Submit</motion.button>
-
-            {/* <ParticleEffectButton
+                      {/* <ParticleEffectButton
         
       >
         BUTTON CONTENT GOES HERE
       </ParticleEffectButton> */}
-
-          </Form> 
-        )}
-      </Formik>
+                    </Form>
+                  )}
+                </Formik>
+              </div>
+            </StylesProvider>
+          </Fade>
+        </div>
       </div>
-      </StylesProvider>
-      </Fade>
-    </div>
     </>
-
   );
 };
 
 FormUserDetails.propTypes = {
   formData: PropTypes.object.isRequired,
   setFormData: PropTypes.func.isRequired,
-  nextStep: PropTypes.func.isRequired
+  nextStep: PropTypes.func.isRequired,
 };
 
 export default FormUserDetails;
--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
-import { List, ListItem, ListItemText } from '@material-ui/core/';
-import Weather from "./weather"
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import { List, ListItem, ListItemText } from "@material-ui/core/";
+import Weather from "./weather";
 // import { Header } from './Header';
 import Quote from "./quote";
 import Time from "./timer";
@@ -11,43 +11,46 @@ import particlesConfig from "./particlesConfig";
 import Particles from "react-particles-js";
 import { motion } from "framer-motion";
 import Fade from "react-reveal/Fade";
+import "./Report.css";
 
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   textCenter: {
-    textAlign: 'center'
+    textAlign: "center",
   },
-//   main: {
-//       width: "100vw"
-//   },
+  //   main: {
+  //       width: "100vw"
+  //   },
   button: {
     // margin: theme.spacing(1),
     // background: 'blue',
     // padding: "5%",
-    marginBottom: "10px",
-  boxShadow: "inset 0px 1px 0px 0px #85c1f5",
-  background: "linear-gradient(to bottom, #61b0ff 5%, #80b5ea 100%)",
-	backgroundColor: "#61b0ff",
-	borderRadius: "6px",
-	border: "1px solid #84bbf3",
-	display: "inline-block",
-	color: "#ffffff",
-	fontSize: "20px",
-	fontWeight: "bold",
-	padding: "8px 30px",
-	textDecoration: "none",
-	textShadow: "0px 1px 0px #528ecc",
+    marginBottom: "2%",
+    boxShadow: "inset 0px 1px 0px 0px #85c1f5",
+    background: "linear-gradient(to bottom, #61b0ff 5%, #80b5ea 100%)",
+    backgroundColor: "#61b0ff",
+    borderRadius: "6px",
+    border: "1px solid #84bbf3",
+    display: "inline-block",
+    color: "#ffffff",
+    fontSize: "20px",
+    fontWeight: "bold",
+    padding: "8px 30px",
+    textDecoration: "none",
+    textShadow: "0px 1px 0px #528ecc",
   },
-//   mainReport: {
-//     height: "100vh"
-//   }
+  //   mainReport: {
+  //     height: "100vh"
+  //   }
 }));
 
 const Confirm = ({ formData, prevStep, nextStep }) => {
   const classes = useStyles();
   const { name, city } = formData;
-  return (<div className={classes.main}><Particles 
-        // style={{position: "absolute"}} height="100vh" width="100vw" 
+  // className={classes.main}
+  return (
+    <div className="reportPage">
+      <Particles
+        // style={{position: "absolute"}} height="100vh" width="100vw"
         // style={{
         //       position: "absolute",
         //       top: 0,
@@ -55,17 +58,20 @@ const Confirm = ({ formData, prevStep, nextStep }) => {
         //       width: "100%",
         //       height: "100%"
         //     }}
-        params={particlesConfig} />
-            <div style={{
+        params={particlesConfig}
+      />
+      <div
+        style={{
           position: "absolute",
           top: 0,
           left: 0,
           width: "100%",
-          height: "100%"
-        }}>
-    <Time name={name}/>
-    <Quote />
-      <div>
+          height: "100%",
+        }}
+      >
+        <Time name={name} />
+        <Quote />
+
         {/* <List>
           <ListItem>
             <ListItemText
@@ -86,6 +92,7 @@ const Confirm = ({ formData, prevStep, nextStep }) => {
         </List> */}
 
         <Weather city={city} />
+        {/* className={classes.textCenter} */}
         <div className={classes.textCenter}>
           {/* <Button
             color='secondary'
@@ -96,14 +103,17 @@ const Confirm = ({ formData, prevStep, nextStep }) => {
             Back
           </Button> */}
           <Fade bottom delay="3000">
-          <motion.button 
-            whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }} 
-    type='submit'   variant='contained'
-    className={classes.button}
-            onClick={() => prevStep()}
-    >Back</motion.button>
-    </Fade>
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              type="submit"
+              variant="contained"
+              className={classes.button}
+              onClick={() => prevStep()}
+            >
+              Back
+            </motion.button>
+          </Fade>
 
           {/* <Button
             color='primary'
@@ -116,17 +126,13 @@ const Confirm = ({ formData, prevStep, nextStep }) => {
         </div>
       </div>
     </div>
-    </div>
   );
 };
 
-
-
 Confirm.propTypes = {
-    formData: PropTypes.object.isRequired,
-    prevStep: PropTypes.func.isRequired,
-    nextStep: PropTypes.func.isRequired
-  };
-  
+  formData: PropTypes.object.isRequired,
+  prevStep: PropTypes.func.isRequired,
+  nextStep: PropTypes.func.isRequired,
+};
 
-export default Confirm
+export default Confirm;
